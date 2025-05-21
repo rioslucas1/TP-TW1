dfddde669f9366f49c46c4334bc2f9fd736280a7
Angel Leyes <angelleyesdk@gmail.com>
2025-05-21 00:35:09 -0300

    se agrego la vista tutores
    se agrego la vista tutores

--- src/main/java/com/tallerwebi/presentacion/ControladorLogin.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorLogin.java
@@ -19,7 +19,7 @@ public class ControladorLogin {
     private ServicioLogin servicioLogin;
 
     @Autowired
-    public ControladorLogin(ServicioLogin servicioLogin){
+    public ControladorLogin(ServicioLogin servicioLogin) {
         this.servicioLogin = servicioLogin;
     }
 
@@ -49,12 +49,12 @@ public class ControladorLogin {
     @RequestMapping(path = "/registrarme", method = RequestMethod.POST)
     public ModelAndView registrarme(@ModelAttribute("usuario") Usuario usuario) {
         ModelMap model = new ModelMap();
-        try{
+        try {
             servicioLogin.registrar(usuario);
-        } catch (UsuarioExistente e){
+        } catch (UsuarioExistente e) {
             model.put("error", "El usuario ya existe");
             return new ModelAndView("nuevo-usuario", model);
-        } catch (Exception e){
+        } catch (Exception e) {
             model.put("error", "Error al registrar el nuevo usuario");
             return new ModelAndView("nuevo-usuario", model);
         }
@@ -83,6 +83,11 @@ public class ControladorLogin {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public ModelAndView inicio() {
         return new ModelAndView("redirect:/login");
+    }
+
+    @RequestMapping("/verTutores")
+    public String verTutores() {
+        return "verTutores";
     }
 }
 
